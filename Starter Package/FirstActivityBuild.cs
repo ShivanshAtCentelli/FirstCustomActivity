a4e8de7d06f56a98be5b3ae07e62f0291acc7524
Shivansh Verma <shivansh.verma@centelli.com>
2023-09-19 12:18:47 +0530

    Added activity to read Excel file and return a workbook object.

--- Starter Package/FirstActivityBuild.cs
+++ Starter Package/FirstActivityBuild.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Activities;
 using System.ComponentModel;
+using Microsoft.Office.Interop.Excel; // Excel
+using System.Runtime.InteropServices; // Marshal
+using System.Reflection; //Missing.Values
 
 namespace StarterPackage.FirstActivityBuild
 {
@@ -33,6 +36,43 @@ namespace StarterPackage.FirstActivityBuild
             string strConcatenatedString = strFirstString + " " + strSecondString;
             ostrOutputString.Set(context, strConcatenatedString);
 
+        }
+    }
+
+    public class CreateWorkbook: CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        [DisplayName("Workbook Path")]
+        [Description("Full path where the workbook is to be created")]
+        public InArgument<string> istrFilePath { get; set; }
+
+        [Category("Output")]
+        [RequiredArgument]
+        [DisplayName("Workbook")]
+        [Description("Workbook object")]
+        public OutArgument<Workbook> owrbkOutputWorkbook { get; set; }
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string strExcelFile = istrFilePath.Get(context);
+
+            // Initialize excel application
+            Application xlApplication = new Application();
+            Workbook xlWorkbook = xlApplication.Workbooks.Add(Missing.Value);
+            xlWorkbook.SaveAs(strExcelFile);
+
+            // Close all application
+            xlWorkbook.Close(true, Missing.Value, Missing.Value);
+            xlApplication.Quit();
+
+            // Output workbook object
+            owrbkOutputWorkbook.Set(context, xlWorkbook);
+
+            // Release COM objects
+            Marshal.ReleaseComObject(xlWorkbook);
+            Marshal.ReleaseComObject(xlApplication);
         }
     }
 }
